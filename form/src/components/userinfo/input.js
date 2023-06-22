4e93af5dd4a308ecd17bc4fecbd4e0326ca70c44
meet_hansalia <meethansalia99@gmail.com>
2023-06-22 15:35:48 +0530

    using formik

--- form/src/components/userinfo/input.js
+++ form/src/components/userinfo/input.js
@@ -1,93 +1,81 @@
 import Card from "../UI/Card";
+
 import Button from "../UI/button";
 import Wrapper from "../wrapper/wrapper";
 import classes from './input.module.css'
 import React, { useState } from 'react';
-import {omit} from 'lodash'
 
 
-const Input=(callback)=>{
+
+const Input=(props)=>{
 
     // const [values, setValues] = useState({});
     const [error, setError] = useState({});
     const [userName, setuserName]=useState("")
     const [userPassword, setUserPassword]= useState("")
+    const [userEmail, setUserEmail]=useState("")
 
-
-
-    // const validation=(event, name, value)=>{
-    //     switch(name){
-            
-    //         case'username':
-    //         if(value.length <= 4){
-    //             setError({
-    //                 ...error,
-    //                 username: 'Username should atleast have 5 letters'
-    //             })
-    //         }else{
-    //             let newObj = omit(error, "username");
-    //             setError(newObj);
-    //         }
-    //         break;
-
-    //         case'email':
-    //         if(!new RegExp( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(value))
-    //         {
-    //             setError({
-    //                 ...error,
-    //                 email:'Enter a valid email address'
-    //             })
-    //         }else{
-    //             let newObj =(error, "email");
-    //             setError(newObj);
-    //         }
-    //         break;
-
-    //         case'password':
-    //         if(!new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/).test(value))
-    //         {
-    //             setError({
-    //                 ...error,
-    //                 password:'Password should contain atleast 8 char and contain uppercase, lowercase and number'
-    //             })
-    //         }else{
-    //             let newObj = omit(error,"password");
-    //             setError(newObj);
-    //         }
-    //         break;
-    //         default: break;
-    //     }
-    // }
-
-    // const handleChange = (event)=>{
-    //     event.preventDefault();
-    //     let name = event.target.name;
-    //     let val = event.target.value;
-
-    //     validation(event, name, val);
-    //     setValues({
-    //         ...values,
-    //         [name]:val,
-    //     })
-    // }
-
+    function isValidEmail(userEmail) {
+        return /\S+@\S+\.\S+/.test(userEmail);
+    }
    
 
-    const handleSubmit = (event) => {
-        if(event) event.preventDefault();
-    
-        if(Object.keys(error).length === 0 && Object.keys(values).length !==0 ){
-           
-    
-        }else{
-            alert("There is an Error!");
+    const inputHandleSubmit = (event) => {
+        event.preventDefault();
+        const enteredName = event.current.value;
+        const enteredEmail= event.current.value;
+        const enteredPassword = event.current.value;
+
+        if(enteredName.trim().length === 0 || enteredEmail.trim() ===0 || enteredPassword.trim() === 0){
+            setError({
+                title: 'Invalid input',
+                message:'Please enter a valid name, Email or Password.'
+            });
+            return;
         }
-    
+       
+        if(!new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)){
+            setError({
+                title:'Invalid Password',
+                message:'Enter a valid password'
+            });
+            return;
+        }
+        setuserName("");
+        setUserEmail("");
+        setUserPassword("");
+        props.onAddUser(enteredName, enteredEmail, enteredPassword);
     }
+    
+    const userNameChangeHandler =(event)=>{
+        setuserName(event.target.value);
+    }
+
+    const userPasswordChangehandler=(event)=>{
+        setUserPassword(event.target.value);
+        
+    }
+
+    const userEmailChangeHandler = (event)=>{
+        if (!isValidEmail(event.target.value)) {
+            setError('Email is invalid');
+          } else {
+            setError(null);
+          }
+      
+          setUserEmail(event.target.value);
+        };
+    
+
+    
+    
+    
     return(
         <Wrapper>
+            {/* {error && <ErrorModel title={error.title} message={error.message} onConfirm={errorhandler}/>} */}
+            
             <Card className={classes.input}>
-                <form onSubmit={handleSubmit}>                
+                <form onSubmit={inputHandleSubmit}>                
                     <label htmlfor="">
                         <h1>Login</h1>
                     </label>             
@@ -95,39 +83,35 @@ const Input=(callback)=>{
                 
                     <label htmlfor="loginUser">User Name</label>
                     <input
-                        onChange={handleChange}
+                        onChange={userNameChangeHandler}
                         type="text"
                         id="loginUser"
                         name="username"
                         placeholder="Enter your Full Name"
+                        value={userName}
                     />               
-                    {
-                        error.username && <h3>{error.username}</h3>
-
-                    }
+                    
                     <label htmlfor="loginEmail">Email</label>
                     <input
-                        onChange={handleChange}
+                        onChange={userEmailChangeHandler}
                         type="text"
                         id="loginpassword"
                         name="password"
                         placeholder="Enter Email Address"
-                    />  
-                    {
-                        error.email && <h3>{error.email}</h3>
-                    }            
+                        value={userEmail}
+                        />  
+                         {error && <h2 style={{color: 'red'}}>{error}</h2>}      
                 
                     <label htmlfor="loginPassword">Password</label>
                     <input
-                        onChange={handleChange}
+                        onChange={userPasswordChangehandler}
                         type="text"
                         id="loginpassword"
                         name="password"
                         placeholder="Enter Your Password"
+                        value={userPassword}
                     />
-                    {
-                        error.password && <h3>{error.password}</h3>
-                    }
+                    
                     <div>
                         <Button type="submit" className="Button">Login</Button>
                     </div>
